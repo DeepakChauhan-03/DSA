46e126c6504162902ab014678d7eb7a5fa4054a8
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-04-03 12:12:00 +0530

    Sum

---
+++ Array/2D/sum.cpp
@@ -0,0 +1,15 @@
+//WAP to find sum of 2d array
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+  int sum = 0;
+   for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+       sum = sum + arr[i][j];
+    }
+   }
+   cout<<"Sum of arr is : "<<sum;
+
+    return 0;
+}
